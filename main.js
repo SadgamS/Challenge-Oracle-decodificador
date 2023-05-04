2eb4123c55cf87f6012a7018308a732d128e993d
SadgamS <brayan010.ba@gmail.com>
2023-05-04 00:51:57 -0400

    Modified the functions and added to do

--- main.js
+++ main.js
@@ -1,42 +1,36 @@
-const llaves = {
-  a: 'ai',
-  e: 'enter',
-  i: 'imes',
-  o: 'ober',
-  u: 'ufat',
-};
 
-const results = {
-    ai: 'a',
-    enter: 'e',
-    imes: 'i',
-    ober: 'o',
-    ufat: 'u',
-}
+const matrizCodigos = [
+    ['a', 'ai'],
+    ['e', 'enter'],
+    ['i', 'imes'],
+    ['o', 'ober'],
+    ['u', 'ufat'],
+];
 
 const input = document.getElementById('texto');
 
-console.log(input);
-
-const button = document.getElementById('encriptar');
-
-
 
 function encriptar(text) {
-    return text.replace(/[aeiou]/g, (match) => llaves[match]);
+    return text.replace(/[aeiou]/g, (match) => matrizCodigos.find(key => key[0] === match)[1]);
 }
 
 function desencriptar(text) {
-    return text.replace(/(ai|enter|imes|ober|ufat)/g, (match) => results[match]);
+    return text.replace(/(ai|enter|imes|ober|ufat)/g, (match) => matrizCodigos.find(key => key[1] === match)[0]);
+}
+// TODO: agregar funcionalidad para limpiar el input y mostrar el resultado en un div
+
+function btnEncriptar() {
+    const text = input.value;
+    const encripted = encriptar(text);
+    input.value = encripted;
 }
 
-
-console.log(encriptar('hola'));
-
-button.addEventListener('click', () => {
+function btnDesencriptar() {
     const text = input.value;
-    console.log(text);
-    const encripted = encriptar(text);
-    console.log(encripted);
-    input.value = encripted;
-});
+    const desencripted = desencriptar(text);
+    input.value = desencripted;
+}
+
+// TODO: agregar funcionalidad al boton de copiar al portapapeles
+console.log(encriptar('hola'));
+console.log(desencriptar('hola'));
