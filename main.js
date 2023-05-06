c3ce6a0b3059c439569f79c45b4f5c3dd94a4037
SadgamS <brayan010.ba@gmail.com>
2023-05-05 22:00:04 -0400

    added new functions and modified functions that were created, added styles, added new things to do

--- main.js
+++ main.js
@@ -1,36 +1,60 @@
-
 const matrizCodigos = [
-    ['a', 'ai'],
-    ['e', 'enter'],
-    ['i', 'imes'],
-    ['o', 'ober'],
-    ['u', 'ufat'],
+  ['a', 'ai'],
+  ['e', 'enter'],
+  ['i', 'imes'],
+  ['o', 'ober'],
+  ['u', 'ufat'],
 ];
 
 const input = document.getElementById('texto');
-
+const result = document.getElementById('resultado');
+const containerObjects = document.getElementById('container-objects');
+const containerResult = document.getElementById('container-objects-results');
+const message = document.getElementById('message');
 
 function encriptar(text) {
-    return text.replace(/[aeiou]/g, (match) => matrizCodigos.find(key => key[0] === match)[1]);
+  return text.replace(
+    /[aeiou]/g,
+    (match) => matrizCodigos.find((key) => key[0] === match)[1],
+  );
 }
 
 function desencriptar(text) {
-    return text.replace(/(ai|enter|imes|ober|ufat)/g, (match) => matrizCodigos.find(key => key[1] === match)[0]);
+  return text.replace(
+    /(ai|enter|imes|ober|ufat)/g,
+    (match) => matrizCodigos.find((key) => key[1] === match)[0],
+  );
 }
-// TODO: agregar funcionalidad para limpiar el input y mostrar el resultado en un div
+
+function btnLimpiar() {
+  input.value = '';
+  result.value = '';
+  containerObjects.style.display = 'flex';
+  containerResult.style.display = 'none';
+}
 
 function btnEncriptar() {
-    const text = input.value;
-    const encripted = encriptar(text);
-    input.value = encripted;
+  const text = input.value;
+  const encripted = encriptar(text);
+  containerObjects.style.display = 'none';
+  containerResult.style.display = 'flex';
+  result.value = encripted;
 }
 
 function btnDesencriptar() {
-    const text = input.value;
-    const desencripted = desencriptar(text);
-    input.value = desencripted;
+  const text = input.value;
+  const desencripted = desencriptar(text);
+  containerObjects.style.display = 'none';
+  containerResult.style.display = 'flex';
+  result.value = desencripted;
 }
 
-// TODO: agregar funcionalidad al boton de copiar al portapapeles
-console.log(encriptar('hola'));
-console.log(desencriptar('hola'));
+
+function btnCopiar() {
+  const text = result.value;
+  navigator.clipboard.writeText(text);
+  message.textContent = 'Copiado al portapapeles';
+  message.style.display = 'block';
+}
+
+// TODO: Agregar detalles...
